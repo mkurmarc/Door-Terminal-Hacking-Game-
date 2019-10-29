05c1f51efca70dfb8a396c87b5786317b145d7c7
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2019-10-29 15:14:19 -0700

    Add files via upload

---
+++ TripleX/TripleX/TripleX.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+void PrintIntroduction(int Difficulty)
+{
+	// welcome message to terminal
+		std::cout << "\n\nYou are an engineer in a colony spaceship hacking a level " << Difficulty << " locked door to the medical bay to\n";
+		std::cout << "retrieve first aid for a life-threatening injury. If you do not break the code, then you will die a bloody death.\n";
+		std::cout << "Your engineer apparatus links to the door terminal where the following information appears...\n\n";
+
+}
+
+void PrintLevelPass(int Difficulty)
+{
+	std::cout << "\n\nThe door terminal security increased to level " << Difficulty << " , and the following information appears...\n";
+}
+
+bool PlayGame(int Difficulty)
+{
+	if (Difficulty == 1)
+	{
+		PrintIntroduction(Difficulty);
+	}
+	else
+	{
+		PrintLevelPass(Difficulty);
+	}
+
+	const int CodeA = 4;
+	const int CodeB = 3;
+	const int CodeC = 2;
+
+	const int CodeSum = CodeA + CodeB + CodeC;
+	const int CodeProduct = CodeA * CodeB * CodeC;
+
+	std::cout << "+ There are 3 numbers in the code";
+	std::cout << "\n+ The codes add up to: " << CodeSum;
+	std::cout << "\n+ The code multiply to give:" << CodeProduct << std::endl;
+
+	// store player's guesses
+	int GuessA, GuessB, GuessC;
+	std::cin >> GuessA >> GuessB >> GuessC;
+	std::cout << "You entered: " << GuessA << GuessB << GuessC;
+
+	int GuessSum = GuessA + GuessB + GuessC;
+
+	int GuessProduct = GuessA * GuessB * GuessC;
+
+	// check if the player's guess is correct
+	if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+	{
+		return true;
+	}
+	else
+	{
+		std::cout << "\nYou bled out and died.";
+		return false;
+	}
+}
+
+int main()
+{
+	int LevelDifficulty = 1;
+	int const MaxDifficulty = 5;
+
+	while (LevelDifficulty <= MaxDifficulty) // loop the game until all levels are completed
+	{
+		bool bLevelComplete = PlayGame(LevelDifficulty);
+		std::cin.clear(); //clears any errors
+		std::cin.ignore(); //discards the buffers
+
+		if (bLevelComplete)
+		{
+			++LevelDifficulty;
+		}
+
+	}
+	std::cout << "The medbay door unlocked and opened.";
+
+	return 0;
+}
